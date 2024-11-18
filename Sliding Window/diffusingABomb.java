fd281fb38ca65b2c7a5ed2b87a9a2c3b0c7ca5f4
AnamikaKumbhare <kumbhare.anamika@gmail.com>
2024-11-18 18:51:59 +0530

    diffusing a bomb

---
+++ Sliding Window/diffusingABomb.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n=code.length;
+        int[] ans=new int[n];
+        if(k==0){
+            Arrays.fill(ans,0);
+            return ans;
+        }
+        int start=1, end=k, sum=0;
+        if(k<0){
+            k=-k;
+            start=n-k;
+            end=n-1;
+        }
+        //initializing for 1st window
+        for (int i = start; i <= end; i++) {
+            sum += code[i % n];
+        }
+        //traverse window for the remaining
+        for(int i=0;i<n;i++){
+            ans[i]=sum;
+            sum-=code[start%n];
+            sum+=code[(end+1)%n];
+            start++;
+            end++;
+        }
+        return ans;
+    }
+}
