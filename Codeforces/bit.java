b5bf6ebdcc43df506ee5f35a257625c69eac90ec
AnamikaKumbhare <kumbhare.anamika@gmail.com>
2024-12-13 20:45:27 +0530

    updated bit++

--- Codeforces/bit.java
+++ Codeforces/bit.java
@@ -1,24 +1,21 @@
-package Codeforces;
-
 import java.util.Scanner;
 
 public class bit {
-    public int bitPP() {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        int n = sc.nextInt(); 
         int x = 0; 
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine(); 
 
         for (int i = 0; i < n; i++) {
-            String operation = sc.nextLine();
-            if (operation.contains("++")) {
-                x++;
-            } else if (operation.contains("--")) {
-                x--;
+            String temp = sc.next(); 
+            if (temp.charAt(1) == '+') { 
+                x++; 
+            } else if (temp.charAt(1) == '-') { 
+                x--; 
             }
         }
 
-        sc.close(); 
-        return x;
+        System.out.println(x); 
+        sc.close();
     }
 }
