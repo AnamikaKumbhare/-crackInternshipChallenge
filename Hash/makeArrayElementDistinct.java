0fcccde6455ace03e763b5360cb60cb05f937b93
AnamikaKumbhare <kumbhare.anamika@gmail.com>
2024-12-22 10:54:26 +0530

    make array elements distinct

---
+++ Hash/makeArrayElementDistinct.java
@@ -0,0 +1,43 @@
+package Hash;
+import java.util.Set;
+import java.util.HashSet;
+class Solution {
+    public int minimumOperations(int[] nums) {
+        if (nums.length == 0) return 0;
+        
+        if (hasAllDistinct(nums, 0, nums.length)) return 0;
+        
+        int operations = 0;
+        int startIndex = 0;
+        
+        while (startIndex < nums.length) {
+           
+            if (nums.length - startIndex < 3) {
+                
+                if (startIndex < nums.length) {
+                    operations++;
+                }
+                break;
+            }
+         
+            startIndex += 3;
+            operations++;
+        
+            if (hasAllDistinct(nums, startIndex, nums.length)) {
+                break;
+            }
+        }
+        
+        return operations;
+    }
+    
+    private boolean hasAllDistinct(int[] nums, int start, int end) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = start; i < end; i++) {
+            if (!set.add(nums[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
